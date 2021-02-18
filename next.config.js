db155361e1f75f21d69746c500c9c0ae45f697ed
Jan Six <six.jan@gmail.com>
2021-02-18 08:33:25 +0100

    add splitbee

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ]
+  },
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import 'nextra-theme-docs/style.css'
+import splitbee from "@splitbee/web"
+import React from "react"
 
 export default function Nextra({ Component, pageProps }) {
+  React.useEffect(() => {
+    splitbee.init({
+      disableCookie: true,
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }

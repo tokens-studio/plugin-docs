1ffa44fe0623065cb20b3b78b777137537bc6620
Robbert Bosker <109062656+rbosker@users.noreply.github.com>
2022-12-30 11:11:02 +0100

    Release 129 docs update (#59)
    * added/adjusted images
    
    * Border token doc en
    
    * Pt-br docs border token (en version)
    
    * dimension token docs pt (en version
    
    * dimension tokens docs
    
    * added new screen for adding sync option
    
    * added screenshot
    
    * added sync image
    
    * updated content and changed analytics
    
    Co-authored-by: Jan Six <six7@github.com>

--- next.config.js
+++ next.config.js
@@ -24,17 +24,5 @@ module.exports = withNextra({
         statusCode: 302
       }
     ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/bee.js",
-        destination: "https://cdn.splitbee.io/sb.js",
-      },
-      {
-        source: "/_hive/:slug",
-        destination: "https://hive.splitbee.io/:slug",
-      },
-    ]
-  },
+  }
 })
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
 import "../styles.css";
 import 'nextra-theme-docs/style.css'
-import splitbee from "@splitbee/web"
 import React from "react"
 
 export default function Nextra({ Component, pageProps }) {
-  React.useEffect(() => {
-    splitbee.init({
-      disableCookie: true,
-      scriptUrl: "/bee.js",
-      apiUrl: "/_hive",
-    })
-  }, [])
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
 }
